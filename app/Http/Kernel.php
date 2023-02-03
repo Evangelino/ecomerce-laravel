2f57ea51c310a8f7f3f305a4329321f1a88522ac
Evangelino Ximenes <ximenesevang@gmail.com>
2023-02-03 17:22:57 +0900

    Update Kernel.php
    Define an authentication middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // My middleware
+        'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
