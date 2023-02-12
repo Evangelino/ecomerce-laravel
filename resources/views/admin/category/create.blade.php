cb7acb0ab54064e0ea3caf955360e35551860e35
Evangelino Ximenes <ximenesevang@gmail.com>
2023-02-13 08:54:54 +0900

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -6,12 +6,12 @@
         <div class="card">
             <div class="card-header">
                 <h3>Add Category
-                    <a href="{{ url('admin/category') }}"
+                    <a href="{{ url('admin/category/') }}"
                         class="btn btn-primary text-white btn-sm btn-danger float-end">BACK</a>
                 </h3>
             </div>
             <div class="card-body">
-                <form action="{{ url('admin/category') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ url('admin/category/') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="row">
                         <div class="col-md-6 mb-3">
@@ -52,7 +52,7 @@
                         </div>
                         <div class="col-md-12 mb-3">
                             <label>Meta Description</label>
-                            <input type="text" name="meta_description" class="form-control">
+                            <textarea name="meta_description" class="form-control" rows="3"></textarea>
                             @error('meta_description') <small class="text-danger">{{ $message }}</small> @enderror
                         </div>
                         <div class="col-md-12 mb-3">
