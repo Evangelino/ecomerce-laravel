747363a2112d9733595ab3a174f5d9b7f7916bd8
Evangelino Ximenes <ximenesevang@gmail.com>
2023-02-13 08:53:37 +0900

    Update CategoryController.php
    Create Store function for creating new Category.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use App\Http\Requests\CategoryFormRequest;
 
 class CategoryController extends Controller
@@ -45,5 +46,41 @@ class CategoryController extends Controller
         $category->save();
 
         return redirect('admin/category')->with('message', 'Category Added Successfully');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update(CategoryFormRequest $request, $category)
+    {
+        $validatedData = $request->validated();
+
+        $category = Category::findOrFail($category);
+        $category->name = $validatedData['name'];
+        $category->slug = Str::slug($validatedData['name']);
+        $category->description = $validatedData['description'];
+
+        $path = 'uploads/category/' . $category->image;
+        if (File::exists($path)) {
+            File::delete($path);
+        }
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+
+            $file->move('uploads/category/', $filename);
+            $category->image = $filename;
+        }
+        $category->meta_title = $validatedData['meta_title'];
+        $category->meta_keyword = $validatedData['meta_keyword'];
+        $category->meta_description = $validatedData['meta_description'];
+
+        $category->status = $request->status == true ? '1' : '0';
+        $category->update();
+        return redirect('admin/category')->with('message', 'Category Updated Successfully');
     }
 }
