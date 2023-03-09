c7ed1b49a75ff8ffeaf892995227328e9c480c07
Evangelino Ximenes <ximenesevang@gmail.com>
2023-03-09 17:03:15 +0900

    Update index.blade.php
    Create and Index page of Brands.

--- resources/views/livewire/admin/brand/index.blade.php
+++ resources/views/livewire/admin/brand/index.blade.php
@@ -1,3 +1,38 @@
 <div>
+    @include('livewire.admin.brand.modal-form')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-header">
+                <h4>
+                    Brands List
+                    <a href="#" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal"
+                        data-bs-target="#addBrandModal">Add Brands</a>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <td>Id</td>
+                            <td>Name</td>
+                            <td>Slug</td>
+                            <td>Status</td>
+                            <td>Action</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Samsung</td>
+                            <td>Samsung</td>
+                            <td>Active</td>
+                            <td>
 
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
