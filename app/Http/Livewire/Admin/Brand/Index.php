0d599addee7e058f2a33e01edf6b27d99803c27d
Evangelino Ximenes <ximenesevang@gmail.com>
2023-03-10 16:57:38 +0900

    Update Index.php
    Brand Index making.

--- app/Http/Livewire/Admin/Brand/Index.php
+++ app/Http/Livewire/Admin/Brand/Index.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Livewire\Admin\Brand;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Str;
 
 class Index extends Component
 {
+
+    public $name, $slug, $status;
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'slug' => 'required|string',
+            'status' => 'nullable'
+        ];
+    }
+
+    public function resetInput()
+    {
+        $this->name = NULL;
+        $this->slug = NULL;
+        $this->status = NULL;
+    }
+
+    public function storeBrand()
+    {
+        $validatedData = $this->validate();
+        Brand::create([
+            'name' => $this->name,
+            // 'slug' => $this->slug,
+            'slug' => $this->Str::slug($this->slug),
+            'status' => $this->status == true ? '1' : '0',
+        ]);
+
+        session()->flash('message', 'Brand added Succesfully');
+        $this->dispatchBrowserEvent('close-modal');
+        $this->resetInput();
+    }
+
     public function render()
     {
         return view('livewire.admin.brand.index')
